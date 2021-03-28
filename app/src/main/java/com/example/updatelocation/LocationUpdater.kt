d429ea46b30f7260cff3f354ef2fe9f7f49f59c8
Jhonata Ávila <jo.avilati@gmail.com>
2021-03-28 18:28:57 -0300

    feat(MainActivity, LocationUpdater): create location request, create geofence, check permission, check gps.

--- app/src/main/java/com/example/updatelocation/LocationUpdater.kt
+++ app/src/main/java/com/example/updatelocation/LocationUpdater.kt
@@ -1,5 +1,6 @@
 package com.example.updatelocation
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
@@ -7,19 +8,28 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Build
+import android.os.HandlerThread
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.MutableLiveData
-import com.google.android.gms.location.Geofence
-import com.google.android.gms.location.GeofencingEvent
+import com.google.android.gms.location.*
 
 private const val TAG = "LocationUpdater"
 
-class LocationUpdater :  LifecycleService() {
+class LocationUpdater : LifecycleService() {
 
+    private lateinit var locationCallBack: LocationCallback
+    private lateinit var geofencingClient: GeofencingClient
+    private lateinit var locationClient: FusedLocationProviderClient
+    private lateinit var pendingBroadcastIntent: PendingIntent
+    private var geoId = ""
+    private lateinit var handlerThread : HandlerThread
 
     companion object {
         private const val NOTIFICATION_TITLE = "Location Updater"
@@ -27,21 +37,18 @@ class LocationUpdater :  LifecycleService() {
         private const val TRACKING_CHANNEL = "tracking_channel"
         private const val TRACKING_NOTIFICATION_ID = 1
         private val exitFromGeofence = MutableLiveData(false)
-        var isTrackingRider: Boolean = false
-            private set
 
         fun startService(context: Context) {
             val startIntent = Intent(context, LocationUpdater::class.java)
             ContextCompat.startForegroundService(context, startIntent)
-            isTrackingRider = true
         }
 
         fun stopService(context: Context) {
             val stopIntent = Intent(context, LocationUpdater::class.java)
             context.stopService(stopIntent)
-            isTrackingRider = false
         }
 
+        //region GEOFENCE
         class GeofenceReceiver : BroadcastReceiver() {
             @SuppressLint("MissingPermission")
             override fun onReceive(context: Context?, intent: Intent?) {
@@ -70,6 +77,24 @@ class LocationUpdater :  LifecycleService() {
                 }
             }
         }
+//        endregion
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        locationClient = LocationServices.getFusedLocationProviderClient(this)
+        geofencingClient = LocationServices.getGeofencingClient(this)
+        val intent = Intent(this, GeofenceReceiver::class.java)
+        pendingBroadcastIntent = PendingIntent.getBroadcast(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        locationCallBack = createLocationCallBack()
+
+        createObservers()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -79,22 +104,135 @@ class LocationUpdater :  LifecycleService() {
         return START_NOT_STICKY
     }
 
-    private fun startNotificationService(){
+    private fun createObservers() {
+        exitFromGeofence.observe(this) {
+
+            updateLocation()
+
+        }
+    }
+
+    private fun updateLocation() {
+        val request = LocationRequest.create().apply {
+            interval = 15000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            isWaitForAccurateLocation = true
+        }
+
+        val permissionFineLocation = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        )
+
+        val builder = LocationSettingsRequest.Builder().apply {
+            addLocationRequest(request)
+        }
+
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task = client.checkLocationSettings(builder.build())
+
+        task.apply {
+            addOnSuccessListener {
+                locationCallBack = createLocationCallBack()
+                handlerThread = HandlerThread("RequestLocation")
+                handlerThread.start()
+                if (permissionFineLocation == PackageManager.PERMISSION_GRANTED) {
+                    locationClient.requestLocationUpdates(
+                            request,
+                            locationCallBack,
+                            handlerThread.looper
+                    ).apply {
+                        addOnSuccessListener {
+                            Log.d(TAG, "REQUEST LOCATION SUCCESS")
+                        }
+                        addOnFailureListener {
+                            Log.d(TAG, "REQUEST LOCATION FAILURE")
+                        }
+                        addOnCanceledListener {
+                            Log.d(TAG, "REQUEST LOCATION CANCELED")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun createLocationCallBack() = object : LocationCallback() {
+        @RequiresApi(Build.VERSION_CODES.O)
+        override fun onLocationResult(locationResult: LocationResult) {
+            val location: Location = locationResult.lastLocation
+            val accuracy = location.verticalAccuracyMeters
+            Log.d(
+                    TAG,
+                    "LOCATION RESULT lat = ${location.latitude} lng = ${location.longitude} accuracy = $accuracy"
+            )
+            createNewGeofence(location)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun createNewGeofence(location: Location) {
+        handlerThread.quit()
+        removeActualGeofence()
+        geoId = "geo_id"
+        val geofence = getGeofence(location)
+        val geofencingRequest = getGeofenceRequest(geofence)
+        initGeofence(geofencingRequest)
+    }
+
+    private fun getGeofenceRequest(geofence: Geofence) =
+            GeofencingRequest.Builder()
+                    .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+                    .addGeofence(geofence)
+                    .build()
+
+    private fun getGeofence(location: Location) = Geofence.Builder()
+            .setCircularRegion(location.latitude, location.longitude, RADIUS)
+            .setRequestId(geoId)
+            .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER or Geofence.GEOFENCE_TRANSITION_EXIT)
+            .setExpirationDuration(Geofence.NEVER_EXPIRE)
+            .setNotificationResponsiveness(0)
+            .build()
+
+    @SuppressLint("MissingPermission")
+    private fun initGeofence(geofencingRequest: GeofencingRequest) {
+        geofencingClient.addGeofences(geofencingRequest, pendingBroadcastIntent)
+                .addOnSuccessListener {
+                    Log.d(TAG, "ADD GEOFENCE SUCCESS")
+                }
+                .addOnFailureListener {
+                    Log.d(TAG, "ADD GEOFENCE FAILS")
+                }
+    }
+
+    private fun removeActualGeofence() {
+        geofencingClient.removeGeofences(mutableListOf(geoId))
+                .addOnFailureListener {
+                    Log.d(TAG, "REMOVE GEOFENCE FAIL")
+                }
+                .addOnSuccessListener {
+                    Log.d(TAG, "REMOVE GEOFENCE SUCCESS")
+                }
+    }
+
+    //region NOTIFICATION SERVICE
+    private fun startNotificationService() {
         val notificationIntent = Intent(this, MainActivity::class.java)
         val pendingIntent = PendingIntent.getActivity(
-            this,
-            0,
-            notificationIntent,
-            0
+                this,
+                0,
+                notificationIntent,
+                0
         )
 
         val notification = NotificationCompat.Builder(this, TRACKING_CHANNEL)
-            .setChannelId(TRACKING_CHANNEL)
-            .setContentTitle(NOTIFICATION_TITLE)
-            .setSmallIcon(R.drawable.ic_baseline_person_pin_circle_24)
-            .setPriority(NotificationManager.IMPORTANCE_DEFAULT)
-            .setContentIntent(pendingIntent)
-            .build()
+                .setChannelId(TRACKING_CHANNEL)
+                .setContentTitle(NOTIFICATION_TITLE)
+                .setSmallIcon(R.drawable.ic_baseline_person_pin_circle_24)
+                .setPriority(NotificationManager.IMPORTANCE_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .build()
 
         startForeground(TRACKING_NOTIFICATION_ID, notification)
     }
@@ -102,17 +240,14 @@ class LocationUpdater :  LifecycleService() {
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val notificationChannel = NotificationChannel(
-                TRACKING_CHANNEL,
-                "Foreground Location Channel",
-                NotificationManager.IMPORTANCE_DEFAULT
+                    TRACKING_CHANNEL,
+                    "Foreground Location Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
             )
             val manager = getSystemService(NotificationManager::class.java)
             assert(manager != null)
             manager.createNotificationChannel(notificationChannel)
         }
     }
-
-
-
-
+//    endregion
 }
--- app/src/main/java/com/example/updatelocation/MainActivity.kt
+++ app/src/main/java/com/example/updatelocation/MainActivity.kt
@@ -1,9 +1,20 @@
 package com.example.updatelocation
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import com.example.updatelocation.databinding.ActivityMainBinding
+import java.util.jar.Manifest
+
+private const val LOCATION_REQUEST = 41
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,18 +29,107 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnStartPauseTracking.setOnClickListener {
-            it as Button
-            if(isTracking) {
-                isTracking = false
-                LocationUpdater.stopService(this)
 
-                it.text = "Start Location Updater"
+            val permissionFineLocation = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            val permissionCoarseLocation = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            if (
+                    permissionFineLocation == PackageManager.PERMISSION_GRANTED &&
+                    permissionCoarseLocation == PackageManager.PERMISSION_GRANTED
+            ) {
+                checkGPSIsON()
             } else {
-                isTracking = true
-                LocationUpdater.startService(this)
-                it.text = "Stop Location Updater"
+                val shouldShowExplanatoryUI = shouldShowRequestPermissionRationale(
+                        android.Manifest.permission.ACCESS_FINE_LOCATION
+                )
+                if (shouldShowExplanatoryUI) {
+                    AlertDialog.Builder(this).apply {
+                        setPositiveButton("Thanks!") { dialog, _ ->
+                            requestPermissionsNeeded()
+                            dialog.dismiss()
+                        }
+                        setTitle("To continue, we need your permission to access your location")
+
+                        setNegativeButton("Cancel") { dialog, _ ->
+                            dialog.dismiss()
+                        }
+                    }
+                } else {
+                    requestPermissionsNeeded()
+                }
+
+            }
+
+        }
+    }
+
+    private fun requestPermissionsNeeded() {
+
+        requestPermissions(
+                arrayOf(
+                        android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                LOCATION_REQUEST
+        )
+    }
+
+    private fun startOrPauseForeground() {
+
+        if (isTracking) {
+            isTracking = false
+            LocationUpdater.stopService(this)
+            binding.btnStartPauseTracking.text = "Start Location Updater"
+        } else {
+            isTracking = true
+            LocationUpdater.startService(this)
+            binding.btnStartPauseTracking.text = "Stop Location Updater"
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            LOCATION_REQUEST -> {
+                if (grantResults.isNotEmpty() &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //TODO Ask to turn on gps
+                } else {
+                    checkGPSIsON()
+                }
             }
         }
+    }
 
+    private fun checkGPSIsON() {
+        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        var gpsEnabled = false
+        var networkEnabled = false
+
+        try {
+            gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch (e: Exception) {
+        }
+
+        try {
+            networkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch (e: Exception) {
+        }
+
+        if (!gpsEnabled && !networkEnabled) {
+            // notify user
+            AlertDialog.Builder(this)
+                    .setMessage("GPS is OFF, please turn on to continue")
+                    .setPositiveButton("Ok") { dialog, _ ->
+                        startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+
+                    }
+                    .setNegativeButton("cancel") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .show();
+        } else {
+            startOrPauseForeground()
+        }
     }
 }
